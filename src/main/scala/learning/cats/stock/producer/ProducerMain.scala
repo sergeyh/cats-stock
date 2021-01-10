1aee3f2622084945b10dd926b6dc74cdaa76d3fd
Sergey Khudyakov <sergeyhudyakov@gmail.com>
2021-01-10 23:40:42 +0300

    Fix producer being re-created

--- src/main/scala/learning/cats/stock/producer/ProducerMain.scala
+++ src/main/scala/learning/cats/stock/producer/ProducerMain.scala
@@ -1,34 +1,32 @@
 package learning.cats.stock.producer
 
-import cats.effect.{ExitCode, IO, IOApp}
-import fs2.Stream
+import cats.effect._
+import fs2.{Stream, kafka}
 import fs2.kafka._
-import io.circe._
 import io.circe.generic.auto._
 import io.circe.syntax._
 
 import scala.concurrent.duration.DurationInt
-import scala.util.Random
 
 object ProducerMain extends IOApp {
 
-  val topic = "stocks"
-
-  val settings = ProducerSettings[IO, String, String]
-    .withBootstrapServers("localhost:9092")
+  val settings: ProducerSettings[IO, String, String] =
+    ProducerSettings[IO, String, String]
+      .withBootstrapServers("localhost:9092")
 
   def run(args: List[String]): IO[ExitCode] = {
-    val stream = for {
-      stock <- Stream.eval(IO { Stock.randomPrice() })
-        .repeat
-      _ <- Stream.sleep(Random.nextInt(5).second)
-      _ <- Stream.eval(IO {
-          val jsonStr = stock.asJson.noSpaces
-          val record = ProducerRecord(topic, stock.symbol, jsonStr)
-          ProducerRecords.one(record)
-        })
-        .through(produce(settings))
-    } yield ()
+    lazy val producer = kafka.produce[IO, String, String, Unit](settings)
+
+    val stream = Stream
+      .eval(IO { Stock.randomPrice() })
+      .repeat
+      .metered(2.seconds)
+      .map { stock =>
+        val jsonStr = stock.asJson.noSpaces
+        val record = ProducerRecord("stocks", stock.symbol, jsonStr)
+        ProducerRecords.one(record)
+      }
+      .through(producer)
     stream.compile.drain.as(ExitCode.Success)
   }
 
